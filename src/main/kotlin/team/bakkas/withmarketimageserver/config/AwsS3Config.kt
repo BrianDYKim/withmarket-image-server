bd7e31a0b5bd3ef63afc77b5e428bd77cb6f28cf
BrianDYKim <18k7102dy@naver.com>
2022-05-26 11:43:39 +0900

    주석 처리

--- src/main/kotlin/team/bakkas/withmarketimageserver/config/AwsS3Config.kt
+++ src/main/kotlin/team/bakkas/withmarketimageserver/config/AwsS3Config.kt
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/** AWS S3에 대한 Config를 수행하는 클래스. 정보들은 모두 config server로부터 가져온다.
+ * @param accessKey S3 Full Access IAM에 대한 access-key 정보
+ * @param secretKey S3 Full Access IAM에 대한 secret-key 정보
+ * @param region Amazon S3를 이용하는 AWS 지역 정보
+ */
 @Configuration
 class AwsS3Config(
     @Value("\${cloud.aws.credentials.access-key}")
